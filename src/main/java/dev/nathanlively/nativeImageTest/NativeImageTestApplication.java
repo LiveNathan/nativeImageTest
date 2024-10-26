81431f4057d11e738cf008a1ea7fdce645c2e8a8
Nathan Lively <60303904+LiveNathan@users.noreply.github.com>
2024-10-26 11:52:26 -0500

    first commit

---
+++ src/main/java/dev/nathanlively/nativeImageTest/NativeImageTestApplication.java
@@ -0,0 +1,13 @@
+package dev.nathanlively.nativeImageTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NativeImageTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NativeImageTestApplication.class, args);
+	}
+
+}
